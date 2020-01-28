2724da0c168d954e9e530e7ba810f40b4eed2278
RC200 <58831048+RC200@users.noreply.github.com>
2020-01-28 11:37:31 -0700

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -15,7 +15,7 @@ LaneChangeBSM = log.PathPlan.LaneChangeBSM
 
 LOG_MPC = os.environ.get('LOG_MPC', False)
 
-LANE_CHANGE_SPEED_MIN = 45 * CV.MPH_TO_MS
+LANE_CHANGE_SPEED_MIN = 35 * CV.MPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
@@ -110,12 +110,12 @@ class PathPlanner():
 
       if lane_change_direction == LaneChangeDirection.left:
         torque_applied = sm['carState'].steeringTorque > 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 2.0 and not lca_left:
-          torque_applied = True # Enable auto LCA only once after 2 sec 
+        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 1.0 and not lca_left:
+          torque_applied = True # Enable auto LCA only once after 1 sec 
       else:
         torque_applied = sm['carState'].steeringTorque < 0 and sm['carState'].steeringPressed
-        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 2.0 and not lca_right:
-          torque_applied = True # Enable auto LCA only once after 2 sec 
+        if CP.autoLcaEnabled and 2.5 > self.pre_auto_LCA_timer > 1.0 and not lca_right:
+          torque_applied = True # Enable auto LCA only once after 1 sec 
 
       lane_change_prob = self.LP.l_lane_change_prob + self.LP.r_lane_change_prob
 
